74347fdb631631b43337f3e51f8cbd4dc97ee4a2
Ethan Brierley <ethanboxx@gmail.com>
2023-10-02 22:32:16 +0100

    basic re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// Ready
+pub use serde_json::{
+    json, to_string, to_string_pretty, to_value, to_vec, to_vec_pretty, to_writer,
+    to_writer_pretty, Map, Number, Serializer, Value,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Maybe
+pub use serde_json::{from_value, Deserializer, StreamDeserializer};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// TODO
+pub use serde_json::{from_reader, from_slice, from_str};
+
+// Complicated (maybe need two)
+pub use serde_json::Error;
