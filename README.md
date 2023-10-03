9b1557349d0aee047d2f0733c6fbe7084e0bace9
Ethan Brierley <ethanboxx@gmail.com>
2023-10-03 23:04:52 +0100

    A few more bits and pieces

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # serde_json_path_to_error
+
+why
+
+- You get data from external service you don't control
+- Data contians PII that mustn't be logged
+- You are already using `serde_json` extensively
+
+why not
+
+- you've benched and de/serializtion is the bottleneck
+- you only want to use path_to_error in a few places
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,108 @@
 // Ready
 pub use serde_json::{
-    json, to_string, to_string_pretty, to_value, to_vec, to_vec_pretty, to_writer,
-    to_writer_pretty, Map, Number, Serializer, Value,
+    json, to_string, to_string_pretty, to_vec, to_vec_pretty, to_writer, to_writer_pretty, Map,
 };
+pub use value::{to_value, Number, Value};
+
+pub use error::{Error, Result};
 
 // Maybe
-pub use serde_json::{from_value, Deserializer, StreamDeserializer};
+pub use de::{Deserializer, StreamDeserializer};
+pub use serde_json::Serializer;
 
-// TODO
-pub use serde_json::{from_reader, from_slice, from_str};
+pub use de::{from_reader, from_slice, from_str};
+pub mod de {
+    // TODO
+    pub use serde_json::de::{Deserializer, IoRead, Read, SliceRead, StrRead, StreamDeserializer};
 
-// Complicated (maybe need two)
-pub use serde_json::Error;
+    use crate::Result;
+
+    use std::io;
+
+    use serde::de::{Deserialize, DeserializeOwned};
+
+    /// Deserialize an instance of type `T` from a string of JSON text.
+    ///
+    /// Equivalent to [serde_json::from_str] but with errors extended with [serde_path_to_error].
+    ///
+    /// See [serde_json::from_str] for more documentation.
+    pub fn from_str<'a, T>(s: &'a str) -> Result<T>
+    where
+        T: Deserialize<'a>,
+    {
+        let jd = &mut serde_json::Deserializer::from_str(s);
+
+        serde_path_to_error::deserialize(jd)
+    }
+
+    /// Deserialize an instance of type `T` from an I/O stream of JSON.
+    ///
+    /// Equivalent to [serde_json::from_reader] but with errors extended with [serde_path_to_error].
+    ///
+    /// See [serde_json::from_reader] for more documentation.
+    pub fn from_reader<R, T>(rdr: R) -> Result<T>
+    where
+        R: io::Read,
+        T: DeserializeOwned,
+    {
+        let jd = &mut serde_json::Deserializer::from_reader(rdr);
+
+        serde_path_to_error::deserialize(jd)
+    }
+
+    /// Deserialize an instance of type `T` from bytes of JSON text.
+    ///
+    /// Equivalent to [serde_json::from_slice] but with errors extended with [serde_path_to_error].
+    ///
+    /// See [serde_json::from_slice] for more documentation.
+    pub fn from_slice<'a, T>(v: &'a [u8]) -> Result<T>
+    where
+        T: Deserialize<'a>,
+    {
+        let jd = &mut serde_json::Deserializer::from_slice(v);
+
+        serde_path_to_error::deserialize(jd)
+    }
+}
+
+/// When serializing or deserializing JSON goes wrong.
+pub mod error {
+    pub use serde_json::error::Category;
+    /// This type represents all possible errors that can occur when serializing or
+    /// deserializing JSON data.
+    pub type Error = serde_path_to_error::Error<serde_json::Error>;
+    /// Alias for a `Result` with the error type `serde_json_path_to_error::Error`.
+    pub type Result<T> = std::result::Result<T, Error>;
+}
+pub mod ser {
+
+    // TODO
+    pub use serde_json::ser::*;
+}
+pub mod map {
+
+    // TODO
+    pub use serde_json::map::*;
+}
+
+pub use value::from_value;
+pub mod value {
+    use serde::de::DeserializeOwned;
+
+    // TODO
+    pub use serde_json::value::{to_value, Index, Number, Serializer, Value};
+
+    use crate::Result;
+
+    /// Interpret a `serde_json::Value` as an instance of type `T`.
+    ///
+    /// Equivalent to [serde_json::from_value] but with errors extended with [serde_path_to_error].
+    ///
+    /// See [serde_json::from_value] for more documentation.
+    pub fn from_value<T>(value: Value) -> Result<T>
+    where
+        T: DeserializeOwned,
+    {
+        serde_path_to_error::deserialize(value)
+    }
+}
